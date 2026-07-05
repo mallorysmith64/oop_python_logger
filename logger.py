0f2a6a1065120cf59365e3b04f58ee00af7d4c77
mallorysmith64 <mallorysmith64@gmail.com>
2026-07-05 13:29:51 -0400

    wrote logs to a newly created csv file with overwrites

--- logger.py
+++ logger.py
@@ -1,11 +1,13 @@
+import csv
 class Logger:
-    def __init__(self, name):
+    def __init__(self, name, filename="log.csv"):
         self.name = name
+        self.filename = filename
         self.history = []
 
     def log(self, level, message):
-        logged_entry = f"{self.name} - {level} - {message}"
-        self.history.append(logged_entry)
+        formatted_entry = f"{self.name} - {level} - {message}"
+        self.history.append([formatted_entry])
         
     def debug(self,message):
         self.log("Debug", message)
@@ -25,6 +27,12 @@ class Logger:
     def show_history(self):
         for entry in self.history:
             print(f"{entry}")
+            
+    def to_csv(self):
+        with open(self.filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["logger", "level", "message"])
+            writer.writerows(self.history)
         
 logging = Logger("SimpleLogger")
 
@@ -35,3 +43,4 @@ error_logging = logging.error("An error occurred")
 critical_logging = logging.critical("A critical message occurred")
 
 logging.show_history()
+writer_logging = logging.to_csv()
